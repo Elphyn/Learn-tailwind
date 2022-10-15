21b99f515255a607f7a9d45a32f6940b661f6fa3
Vlad <elphynkarelov@gmail.com>
2022-10-15 20:11:43 +0900

    halfway there

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import girl from './Girl.png'
+import email from './Mail.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center bg-secondary h-screen items-center">
+      <Card>
+        <Info/>
+        <Button/>
+      </Card>
     </div>
   );
+}
+
+function Card(props){
+  return(
+    <div className="w-1/5 bg-gray-100 h-[90%] rounded-2xl">
+      {props.children}
+    </div>
+  )
+}
+
+function Info(){
+  return(
+    <div className="flex-col" >
+      <img src={girl}/>
+      <h1 className='text-center'>Laura Smith</h1>
+      <h3 className='text-center text-thrid'>Frontend Developer</h3>
+      <h4 className='text-center'>laurasmith.website</h4>
+    </div>
+  );
+}
+
+function Button(){
+  return(
+    <div className='flex justify-center'>
+      <a href='#' className='flex justify-center items-center border shadow w-2/3 rounded'>
+        <img src={email}/>
+        Email
+      </a>
+    </div>
+    
+  )
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaly: '#23252C',
+        secondary: '#1A1B21',
+        thrid: '#F47D27',
+      }
+    },
+  },
+  plugins: [],
+}
